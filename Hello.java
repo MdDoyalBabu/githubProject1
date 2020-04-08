0ce8ee751eb873340dfb559cab53d2210e254081
MdDoyalBabu <mddoyalbabu2020>
2020-04-08 21:34:17 +0600

    Added a new method callded Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("VS code is Awsome....");
+        greating("MD Doyal");
+    }
+
+    public static void greating(String name){
+        System.out.println("Welcome "=name)
     }
 }
